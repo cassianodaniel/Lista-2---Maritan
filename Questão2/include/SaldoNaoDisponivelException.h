b067cd2227dc8aaafbdb8be5e944a12b6880c9e4
Daniel Chaves <52015731+danielcassiano@users.noreply.github.com>
2020-03-14 16:59:35 -0300

    Add files via upload

---
+++ Questão2/include/SaldoNaoDisponivelException.h
@@ -0,0 +1,17 @@
+#ifndef SALDONAODISPONIVELEXCEPTION_H
+#define SALDONAODISPONIVELEXCEPTION_H
+#include <exception>
+
+using namespace std;
+
+class SaldoNaoDisponivelException : public exception
+{
+    public:
+        SaldoNaoDisponivelException();
+
+    protected:
+
+    private:
+};
+
+#endif // SALDONAODISPONIVELEXCEPTION_H
